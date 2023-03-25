48029fe0afdf259261758c4622d0b5b33083e3bd
ted-austral <eduardo.martinez@ing.austral.edu.ar>
2023-03-25 01:12:55 -0300

    Minor changes in src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,34 @@
-#include <Arduino.h>
 
 /*
- *  Project 01-blink_01
+ *  Project 01-blink_01 - file: main.cpp
  *      Blinks a LED changing blink frequency by pushing a key
  *      Key connected to ground, so program must add pullup resistor
  *      Prints delay in milliseconds
  *      Austral 2023 - Electronica Digital - EAM
  */
 
+#include <Arduino.h>
+
 #define LONG_DELAY  1000
 #define SHORT_DELAY 100
 
+/*
+ *  values defined in platformio.ini
+ *      LED for GPIO where is connected led
+ *      PUSH for GPIO where is connected tact switch
+ *      BAUD communication speed for Serial object
+ */
 
 int old_val = 0;    //  variable to remember present delay
 
 void
 setup(void)
 {
-    pinMode(LED,OUTPUT);
-    pinMode(PUSH,INPUT_PULLUP);
-    digitalWrite(LED,LOW);
-    Serial.begin(BAUD);
-    old_val = LONG_DELAY;
+    pinMode(LED,OUTPUT);            // GPIO 'LED' as OUTPUT
+    pinMode(PUSH,INPUT_PULLUP);     // GPIO 'PUSH' as INPUT connecting internally a pullup resistor
+    digitalWrite(LED,LOW);          // Write on 'LED' a LOW (0 volt) to unlit led
+    Serial.begin(BAUD);             // Initialize Serial object with BAUD number
+    old_val = LONG_DELAY;           // Set new value LONG_DELAY to old_val
 }
 
 void
@@ -29,8 +36,7 @@ loop(void)
 {
 #if 1       // High level programming way. Change to '#if 0' to use alternate code, save and compile
 
-    int delay_val;
-    int led_now;
+    int delay_val, led_now;
 
     led_now = digitalRead(LED);                 //  LED state now !!
     digitalWrite(LED,!led_now);                 //  Change to other state
